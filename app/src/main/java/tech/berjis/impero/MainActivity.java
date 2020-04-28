01ba1974015ac21784e6579dd825ff15fd1e3154
BerjisTech <berjistechnologies@gmail.com>
2020-04-28 23:38:20 +0300

    My Profile UI

--- app/src/main/java/tech/berjis/impero/MainActivity.java
+++ app/src/main/java/tech/berjis/impero/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (restorePrefData()) {
 
-            Intent mainActivity = new Intent(getApplicationContext(),FeedActivity.class );
+            Intent mainActivity = new Intent(getApplicationContext(),ProfileActivity.class );
             startActivity(mainActivity);
             finish();
 
