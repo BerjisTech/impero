d143a4e507825bfe7ef679a147ad4213d0e7d0b1
BerjisTech <berjistechnologies@gmail.com>
2020-04-30 02:01:39 +0300

    Comments UI

--- app/src/main/java/tech/berjis/impero/MainActivity.java
+++ app/src/main/java/tech/berjis/impero/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (restorePrefData()) {
 
-            Intent mainActivity = new Intent(getApplicationContext(), ChangePhoneNumber.class );
+            Intent mainActivity = new Intent(getApplicationContext(), CommentsActivity.class );
             startActivity(mainActivity);
             finish();
 
